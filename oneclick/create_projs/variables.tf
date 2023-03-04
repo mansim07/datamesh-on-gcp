556c9ec54aae167d2bf7153c839dad38160bf3a4
Jitendra <jitendrayadav@google.com>
2023-03-04 14:47:46 +0000

    infra setup changes

---
+++ oneclick/create_projs/variables.tf
@@ -0,0 +1,12 @@
+variable "project_datagov" {
+  type = string
+}
+variable "project_datasto" {
+  type = string
+}
+variable "org_id" {
+  type = string
+}
+variable "billing_id" {
+  type = string
+}
